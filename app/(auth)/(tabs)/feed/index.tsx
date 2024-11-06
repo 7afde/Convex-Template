e9738eff61857dd48883e82749555ab920c7881a
hafed <hafed.zouyed@univ-constantine2.dz>
2024-11-06 13:54:49 +0100

    setting up sentry

--- app/(auth)/(tabs)/feed/index.tsx
+++ app/(auth)/(tabs)/feed/index.tsx
@@ -1,10 +1,27 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, Button } from "react-native";
+import * as Sentry from "@sentry/react-native";
 
 const Page = () => {
+  const testError = () => {
+    try {
+      throw new Error("Test Error");
+    } catch (error) {
+      const sentryID = Sentry.captureMessage("problem");
+      console.log("sentryID :", sentryID);
+
+      const userFeedback: Sentry.UserFeedback = {
+        event_id: sentryID,
+        name: "John Doe",
+        email: "zouoyedhafed00@gmail.com",
+        comments: "This is a test feedback",
+      };
+
+      Sentry.captureUserFeedback(userFeedback);
+    }
+  };
   return (
-    <View>
-      <Text>Page</Text>
+    <View className="flex-1 items-center justify-center">
+      <Button onPress={testError} title="Error" />
     </View>
   );
 };
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,10 @@
 import { router, Slot, useSegments } from "expo-router";
-import { ClerkProvider, ClerkLoaded, useAuth } from "@clerk/clerk-expo";
+import {
+  ClerkProvider,
+  ClerkLoaded,
+  useAuth,
+  useUser,
+} from "@clerk/clerk-expo";
 import { tokenCache } from "@/utils/cache";
 import { LogBox } from "react-native";
 import {
@@ -12,6 +17,7 @@ import { useEffect } from "react";
 import * as SplashScreen from "expo-splash-screen";
 import { ConvexReactClient } from "convex/react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
+import * as Sentry from "@sentry/react-native";
 import "../global.css";
 
 const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
@@ -28,6 +34,31 @@ LogBox.ignoreLogs(["Clerk: Clerk has been loaded with development keys"]);
 
 SplashScreen.preventAutoHideAsync();
 
+// Construct a new instrumentation instance. This is needed to communicate between the integration and React
+const routingInstrumentation = new Sentry.ReactNavigationInstrumentation();
+
+Sentry.init({
+  dsn: process.env.EXPO_PUBLIC_SENTRY_DSN,
+  attachScreenshot: true,
+  debug: false, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
+  tracesSampleRate: 1.0,
+  _experiments: {
+    // Here, we'll capture profiles for 100% of transactions.
+    profilesSampleRate: 1.0,
+    // Session replays
+    replaysSessionSampleRate: 1.0,
+    replaysOnErrorSampleRate: 1.0,
+  },
+  integrations: [
+    new Sentry.ReactNativeTracing({
+      // Pass instrumentation to be used as `routingInstrumentation`
+      routingInstrumentation,
+      enableNativeFramesTracking: true,
+    }),
+    Sentry.mobileReplayIntegration(),
+  ],
+});
+
 const InitialLayout = () => {
   const [fontsLoaded] = useFonts({
     DMSans_400Regular,
@@ -36,6 +67,7 @@ const InitialLayout = () => {
   });
   const { isLoaded, isSignedIn } = useAuth();
   const segments = useSegments();
+  const user = useUser();
 
   useEffect(() => {
     if (fontsLoaded) {
@@ -55,10 +87,21 @@ const InitialLayout = () => {
     }
   }, [isSignedIn]);
 
+  useEffect(() => {
+    if (user && user.user) {
+      Sentry.setUser({
+        id: user.user.id,
+        email: user.user.emailAddresses[0].emailAddress,
+      });
+    } else {
+      Sentry.setUser(null);
+    }
+  }, [user]);
+
   return <Slot />;
 };
 
-export default function RootLayout() {
+const RootLayout = () => {
   return (
     <ClerkProvider publishableKey={publishableKey!} tokenCache={tokenCache}>
       <ClerkLoaded>
@@ -68,4 +111,6 @@ export default function RootLayout() {
       </ClerkLoaded>
     </ClerkProvider>
   );
-}
+};
+
+export default Sentry.wrap(RootLayout);
